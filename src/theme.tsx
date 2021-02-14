69bb2149fba5af4e75b65b9b9a89397820a07d07
Vangelis Bouzoukas <vangelis.b@icloud.com>
2021-02-13 16:59:37 -0800

    build: update dependencies

--- src/theme.tsx
+++ src/theme.tsx
@@ -1,21 +1,6 @@
-import { extendTheme } from '@chakra-ui/react'
-import { createBreakpoints } from '@chakra-ui/theme-tools'
+import { extendTheme } from "@chakra-ui/react";
 
-const fonts = { mono: `'Menlo', monospace` }
+// TODO: Override default Chakra UI theme.
+const theme = extendTheme({});
 
-const breakpoints = createBreakpoints({
-  sm: '40em',
-  md: '52em',
-  lg: '64em',
-  xl: '80em',
-})
-
-const theme = extendTheme({
-  colors: {
-    black: '#16161D',
-  },
-  fonts,
-  breakpoints,
-})
-
-export default theme
+export default theme;
