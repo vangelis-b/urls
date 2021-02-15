9142636961474adac117fe377ecd5c2085912414
Vangelis Bouzoukas <vangelis.b@icloud.com>
2021-02-14 22:42:02 -0800

    refactor: change children ts type to reactnode

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -3,7 +3,7 @@ import Footer from './Footer'
 import Header from './Header'
 
 interface MainLayoutProps {
-  children?: JSX.Element
+  children?: React.ReactNode
 }
 
 const MainLayout = ({children}: MainLayoutProps): JSX.Element => {
