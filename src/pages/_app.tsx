815530484cdbb7d3712abfb498889f3c1523e832
Vangelis Bouzoukas <vangelis.b@icloud.com>
2021-02-14 22:51:13 -0800

    fix: head title placement

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,14 @@
 import {AppProps} from 'next/app'
 import {ChakraProvider} from '@chakra-ui/react'
+import {Head} from 'next/document'
 import theme from '../theme'
 
 const MyApp = ({Component, pageProps}: AppProps): JSX.Element => {
   return (
     <ChakraProvider resetCSS theme={theme}>
+      <Head>
+        <title>_urls</title>
+      </Head>
       <Component {...pageProps} />
     </ChakraProvider>
   )
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,9 +5,7 @@ export default class Document extends NextDocument {
   render(): JSX.Element {
     return (
       <Html>
-        <Head>
-          <title>_urls</title>
-        </Head>
+        <Head />
         <body>
           {/* Make Color mode to persists when you refresh the page. */}
           <ColorModeScript />
