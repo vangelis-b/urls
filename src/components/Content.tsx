1abf6b195fa79c9b0b1a5e30d74d580269a71060
Vangelis Bouzoukas <vangelis.b@icloud.com>
2021-02-15 17:47:31 -0800

    feat: add work in progress notice

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,10 +1,18 @@
-import {Container, SimpleGrid} from '@chakra-ui/react'
+import {Alert, AlertIcon, Container, SimpleGrid, Text} from '@chakra-ui/react'
 import UrlsSection from './UrlsSection'
 import sources from '../constants/sources'
 
 const Content = (): JSX.Element => {
   return (
     <Container maxW={1200} p={[4, 8]}>
+      {/* TODO: Remove alert when there is real data. */}
+      <Alert mb={[4, 8]} colorScheme="cyan">
+        <AlertIcon />
+        <Text fontWeight="bold" mr={1} color="cyan.700">
+          _urls
+        </Text>
+        <Text>is still in construction.</Text>
+      </Alert>
       <SimpleGrid columns={[1, 1, 2, 3]} spacing={[4, 8]}>
         {sources.map(({title, externalSourceUrl}) => {
           return (
