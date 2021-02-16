da7dc1e42fb1f8c705b16ee95124af436b76c281
Vangelis Bouzoukas <vangelis.b@icloud.com>
2021-02-16 15:16:44 -0800

    style: eslint rule to enforce prop sorting

--- .eslintrc.js
+++ .eslintrc.js
@@ -35,6 +35,7 @@ module.exports = {
     '@typescript-eslint/no-var-requires': 'off',
     'no-console': ['error', {allow: ['warn', 'error']}],
     'react/display-name': 'off',
+    'react/jsx-sort-props': 'error',
     'react/prop-types': 'off',
     'react/react-in-jsx-scope': 'off',
   },
