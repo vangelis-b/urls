9452f11124c64157d390e5cece6db73a9885ef7c
Vangelis Bouzoukas <vangelis.b@icloud.com>
2021-02-17 17:39:57 -0800

    feat: show coming soon info messages in sections

--- src/components/urls/UrlsSectionContent.tsx
+++ src/components/urls/UrlsSectionContent.tsx
@@ -1,4 +1,4 @@
-import {Box, Divider, SkeletonText} from '@chakra-ui/react'
+import {Alert, Box, Divider, SkeletonText, Text} from '@chakra-ui/react'
 import UrlLink from './UrlLink'
 
 const UrlsSectionContent = (): JSX.Element => {
@@ -6,13 +6,15 @@ const UrlsSectionContent = (): JSX.Element => {
   const data: Array<number> = []
   // const data = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
   const description = 'How Tim Cook transformed Apple after Steve Jobs'
-  const isLoading = true
+  const isLoading = false
+
+  if (isLoading) {
+    return <SkeletonText mt={5} noOfLines={10} spacing={5} />
+  }
 
   return (
     <Box mx={5} my={5}>
-      {isLoading ? (
-        <SkeletonText mt={5} noOfLines={10} spacing={5} />
-      ) : (
+      {data && data.length > 0 ? (
         data.map((url, index) => {
           const isLastUrl = data.length === index + 1
 
@@ -23,6 +25,10 @@ const UrlsSectionContent = (): JSX.Element => {
             </Box>
           )
         })
+      ) : (
+        <Alert colorScheme="cyan" mb={[4, 8]}>
+          <Text>Coming soon.</Text>
+        </Alert>
       )}
     </Box>
   )
