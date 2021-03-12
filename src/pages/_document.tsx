96dc4a970ff45e14e9ac48ad53f77ae47c6fe202
Vangelis Bouzoukas <vangelis.b@icloud.com>
2021-03-12 00:12:30 -0800

    fix: content security policy for images

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,7 +2,7 @@ import NextDocument, {Html, Head, Main, NextScript} from 'next/document'
 import {ColorModeScript} from '@chakra-ui/react'
 
 const CSP =
-  "default-src self; connect-src 'self' api.rss2json.com; img-src 'self' a.fsdn.com a.thumbs.redditmedia.com b.thumbs.redditmedia.com media.wired.com www.techmeme.com; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; base-uri self; form-action self"
+  "default-src self; connect-src 'self' api.rss2json.com; img-src 'self' data:; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'"
 
 export default class Document extends NextDocument {
   render(): JSX.Element {
