b06d1bbe3ff7354e0f5d39fab36ba15a5924aa31
Vangelis Bouzoukas <vangelis.b@icloud.com>
2021-03-11 23:55:03 -0800

    fix: add content security policy as meta tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,11 +1,16 @@
 import NextDocument, {Html, Head, Main, NextScript} from 'next/document'
 import {ColorModeScript} from '@chakra-ui/react'
 
+const CSP =
+  "default-src self; connect-src 'self' api.rss2json.com; img-src 'self' a.fsdn.com a.thumbs.redditmedia.com b.thumbs.redditmedia.com media.wired.com www.techmeme.com; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; base-uri self; form-action self"
+
 export default class Document extends NextDocument {
   render(): JSX.Element {
     return (
       <Html>
-        <Head />
+        <Head>
+          <meta httpEquiv="Content-Security-Policy" content={CSP} />
+        </Head>
         <body>
           {/* Make Color mode to persists when you refresh the page. */}
           <ColorModeScript initialColorMode="system" />
