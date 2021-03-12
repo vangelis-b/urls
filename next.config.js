55084c3e883b86e29187492aa7798b3c7612a85f
Vangelis Bouzoukas <vangelis.b@icloud.com>
2021-03-11 21:32:48 -0800

    feat: implement nextjs server secure headers

--- next.config.js
+++ next.config.js
@@ -3,7 +3,35 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 })
 
 module.exports = withBundleAnalyzer({
-  reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=63072000; includeSubDomains; preload',
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'deny',
+          },
+          {
+            key: 'X-Download-Options',
+            value: 'noopen',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1',
+          },
+        ],
+      },
+    ]
+  },
   images: {
     domains: [
       'a.fsdn.com',
@@ -13,4 +41,6 @@ module.exports = withBundleAnalyzer({
       'www.techmeme.com',
     ],
   },
+  poweredByHeader: false,
+  reactStrictMode: true,
 })
