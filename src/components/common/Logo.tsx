e2b5a42a3342bc7d29d7753dd1d0af32e3e3f745
Vangelis Bouzoukas <vangelis.b@icloud.com>
2021-03-14 16:30:20 -0700

    refactor: make logo button an anchor

--- src/components/common/Logo.tsx
+++ src/components/common/Logo.tsx
@@ -1,14 +1,17 @@
-import {Button} from '@chakra-ui/react'
 import NextLink from 'next/link'
+import splitbee from '@splitbee/web'
+import {Button} from '@chakra-ui/react'
 
 const Logo = (): JSX.Element => {
   return (
-    <NextLink href="/">
+    <NextLink href="/" passHref>
       <Button
+        aria-label="_urls logo"
+        as="a"
         color="cyan.600"
-        data-splitbee-event="Go to Homepage"
         fontSize="x-large"
         fontWeight="bold"
+        onClick={() => splitbee.track('Go to Homepage')}
         variant="ghost"
       >
         _urls
