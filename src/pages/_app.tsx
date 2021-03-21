88cb73a15403c0c10eed5f9725772288f61226d6
Vangelis Bouzoukas <vangelis.b@icloud.com>
2021-03-20 22:35:59 -0700

    feat: extensive favicon size/type support

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -19,9 +19,27 @@ const App = ({Component, pageProps}: AppProps): JSX.Element => {
   return (
     <ChakraProvider theme={theme}>
       <Head>
-        <link href="favicon.png" rel="icon" type="image/png" />
-        <link color="#0987A0" href="favicon.svg" rel="mask-icon" />
-        <link href="favicon.svg" rel="icon" />
+        <link rel="apple-touch-icon" sizes="57x57" href="/icons/favicon-57x57.png" />
+        <link rel="apple-touch-icon" sizes="60x60" href="/icons/favicon-60x60.png" />
+        <link rel="apple-touch-icon" sizes="72x72" href="/icons/favicon-72x72.png" />
+        <link rel="apple-touch-icon" sizes="76x76" href="/icons/favicon-76x76.png" />
+        <link rel="apple-touch-icon" sizes="114x114" href="/icons/favicon-114x114.png" />
+        <link rel="apple-touch-icon" sizes="120x120" href="/icons/favicon-120x120.png" />
+        <link rel="apple-touch-icon" sizes="144x144" href="/icons/favicon-144x144.png" />
+        <link rel="apple-touch-icon" sizes="152x152" href="/icons/favicon-152x152.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/icons/favicon-180x180.png" />
+        <link rel="icon" type="image/svg+xml" href="/icons/favicon.svg" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="96x96" href="/icons/favicon-96x96.png" />
+        <link rel="icon" type="image/png" sizes="192x192" href="/icons/favicon-192x192.png" />
+        <link rel="shortcut icon" type="image/x-icon" href="/icons/favicon.ico" />
+        <link rel="icon" type="image/x-icon" href="/icons/favicon.ico" />
+        <meta name="msapplication-TileColor" content="#00A3C4" />
+        <meta name="msapplication-TileImage" content="/icons/favicon-144x144.png" />
+        <meta name="msapplication-config" content="/browserconfig.xml" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#00A3C4" />
       </Head>
       <SWRConfig value={{fetcher: fetcher}}>
         <Component {...pageProps} />
