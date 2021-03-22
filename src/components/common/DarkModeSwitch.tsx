18164ae1a33255f3313fee2538ff2f2554d4e1db
Vangelis Bouzoukas <vangelis.b@icloud.com>
2021-03-21 18:34:46 -0700

    style: rename util that capitalizes first char

--- src/components/common/DarkModeSwitch.tsx
+++ src/components/common/DarkModeSwitch.tsx
@@ -1,6 +1,6 @@
 import {MoonIcon, SunIcon} from '@chakra-ui/icons'
 import {IconButton, Tooltip, useColorMode, useColorModeValue} from '@chakra-ui/react'
-import capitalize from '../../utils/capitalize'
+import capitalizeFirstChar from '../../utils/capitalizeFirstChar'
 
 const DarkModeSwitch = (): JSX.Element => {
   const {toggleColorMode} = useColorMode()
@@ -13,7 +13,7 @@ const DarkModeSwitch = (): JSX.Element => {
     <Tooltip label={label} openDelay={500}>
       <IconButton
         aria-label={label}
-        data-splitbee-event-mode={capitalize(nextColorModeLabel)}
+        data-splitbee-event-mode={capitalizeFirstChar(nextColorModeLabel)}
         data-splitbee-event="Toggle Dark Mode"
         icon={<SwitchIcon />}
         onClick={toggleColorMode}
