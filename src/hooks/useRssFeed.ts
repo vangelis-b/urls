520862fbb74c81e370057f08778f9e44364ebae4
Vangelis Bouzoukas <vangelis.b@icloud.com>
2021-03-28 14:03:46 -0700

    fix: maintain top news feed item order

--- src/hooks/useRssFeed.ts
+++ src/hooks/useRssFeed.ts
@@ -1,18 +1,12 @@
 import useSWR from 'swr'
 import RssFeed from '../types/RssFeed'
 import rssFeedFetcher from '../utils/rssFeedFetcher'
-import sortRssFeedItemsByIsoDate from '../utils/sortRssFeedItemsByIsoDate'
 
 const useRssFeed = (url: string): any => {
   const {data, error} = useSWR<RssFeed>(url, rssFeedFetcher)
 
-  let preparedData = data
-  if (data) {
-    preparedData = {...data, items: sortRssFeedItemsByIsoDate(data.items)}
-  }
-
   return {
-    data: preparedData,
+    data,
     isError: error,
     isLoading: !error && !data,
   }
